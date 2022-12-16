a9923b1e74dc2944c4242600d50c997a8b8fad84
Wesley Crus <sistemaswesley@gmail.com>
2022-12-16 16:24:35 -0300

    Bot para Cadastro Moedas Protheus

---
+++ app.py
@@ -0,0 +1,57 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(958,1070,duration=0.5)
+#pyautogui.click(830,500,duration=0.5)
+#pyautogui.write('SIGAMDI')
+#pyautogui.click(830,565,duration=0.5)
+#pyautogui.write('teste')
+#pyautogui.click(830,625,duration=0.5)
+#pyautogui.write('teste')
+pyautogui.sleep(5)
+pyautogui.click(983,667,duration=0.5)
+pyautogui.sleep(3)
+
+pyautogui.click(818,537,duration=0.5)
+pyautogui.write('wesley.crus')
+pyautogui.click(813,601,duration=0.5)
+pyautogui.write('admroot')
+pyautogui.click(1072,727,duration=0.5)
+pyautogui.sleep(1)
+
+#pyautogui.click(813,518,duration=0.5)
+#pyautogui.write('02')
+
+pyautogui.click(1068,752,duration=0.5)
+pyautogui.sleep(4)
+
+#Atualizacoes
+pyautogui.click(37,465,duration=0.5)
+pyautogui.sleep(0.5)
+
+#Cadastro - Moedas
+pyautogui.click(1255,174,duration=0.5)
+pyautogui.sleep(1)
+
+#Cadastro - Moedas Botão Confirmar (Parametros Sistema)
+pyautogui.click(1061,712,duration=0.5)
+pyautogui.sleep(12)
+
+#Cadastro - Moedas Botão Incluir Moeda
+pyautogui.click(46,150,duration=0.5)
+pyautogui.sleep(3)
+
+##Cadastro - Moedas Campo: Data
+pyautogui.click(72,189,duration=0.5)
+pyautogui.write('17122022')
+
+##Cadastro - Moedas Campo: Moeda02
+pyautogui.click(475,188,duration=0.5)
+pyautogui.write('1,5898')
+
+##Cadastro - Moedas Botão Confirma Inclusão
+pyautogui.click(1863,121,duration=0.5)
+pyautogui.sleep(1)
+
+##Cadastro - Moedas Botão Fechar Registro Salvao com sucesso
+pyautogui.click(1161,626,duration=0.5)
